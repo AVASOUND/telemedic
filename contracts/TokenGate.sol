5875b945ba2e050c7dae46dbf265ed88ea3752c5
Karthik <33004854+Karthi1908@users.noreply.github.com>
2023-06-14 11:45:43 +0800

    changes to newWebinar and addPatient function
    changed ownerOnly access for addWebinar and addPatient functions

--- contracts/TokenGate.sol
+++ contracts/TokenGate.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "./PatientRecord.sol";
 import "./RecordAccessToken.sol";
 
-contract TokenGate is Ownable , ReentrancyGuard {
+contract TokenGate is Ownable , ReentrancyGuard, ERC721Holder {
 
     PatientRecord public PRT;
     RecordAccessToken public RAT;
@@ -36,8 +37,7 @@ contract TokenGate is Ownable , ReentrancyGuard {
 
 
 
-    mapping (uint => PatientDetails ) public patients;
-    mapping ( address => uint) public walletIdMap;
+    mapping (address => PatientDetails ) public patients;
     mapping (string => Token) public tokens;
     mapping( uint => WebinarDetails) public webinars;
     mapping( address => mapping( string => uint)) public balance;
@@ -56,14 +56,14 @@ contract TokenGate is Ownable , ReentrancyGuard {
         
     }
 
-    function addPatient(string memory _fname, string memory _lname, address _wallet )public onlyOwner {
+    function addPatient(string memory _fname, string memory _lname)public  {
         patientId ++;
-        patients[patientId] = PatientDetails(_fname, _lname, _wallet);
-        walletIdMap[_wallet] = patientId;
+        patients[msg.sender] = PatientDetails(_fname, _lname, msg.sender);
+        
 
     }
 
-    function mintPatientDiagnosis (uint _patientId , uint _appointmentId , string memory _uriOfDiagnosis) public onlyOwner {
+    function mintPatientDiagnosis (address _patientId , uint _appointmentId , string memory _uriOfDiagnosis) public onlyOwner {
         require(patients[_patientId].wallet != address(0), "Invalid Patient Id");
         PRT.safeMint(patients[_patientId].wallet, _appointmentId, _uriOfDiagnosis);
         RAT.mint(patients[_patientId].wallet, _appointmentId, 1, '0x00');
@@ -75,16 +75,18 @@ contract TokenGate is Ownable , ReentrancyGuard {
         RAT.mint(_to, _tokenId, 1, '0x00');
     }
 
-    function newWebinar(string memory _topic, string memory _presenter, uint _time, string memory _currency, uint _amount ) public onlyOwner {
+    function newWebinar(string memory _topic, string memory _presenter, uint _time, string memory _currency, uint _amount, string memory uri ) public returns(uint) {
         
         webinarId++;
         webinars[webinarId] = WebinarDetails( _topic, _presenter, _time, _currency, _amount);
-        PRT.safeMint(address(this), webinarId, "");
+        PRT.safeMint(address(this), webinarId, uri);
+
+        return webinarId;
 
     }
 
     function enrolWebinar(uint _webinarId) public {
-        require(walletIdMap[msg.sender] != 0, "Patient Need to register");
+        
         require(webinars[_webinarId].time != 0, " Check Webinar Id");
         string memory currency = webinars[webinarId].currency;
         IERC20 tokenContract_ = tokens[currency].tokenAddress;
