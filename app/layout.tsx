57429931c8cdfaaf776e362fdcb18a614a381022
Dominic Leon Hackett <dominichackett@gmail.com>
2023-06-14 09:16:18 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,10 @@ import {
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+/*export const metadata = {
   title: "Telemedic App",
   description: "Created by Replicator Labs",
-};
+};*/
 
 // Configure chains & providers
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
